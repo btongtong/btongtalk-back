8b9857ae9d9c97e72149f3120179be32378fbdb0
btongtong <leeym2615@naver.com>
2024-10-24 15:42:14 +0900

    style: Delete not used import package

--- src/main/java/btongtong/btongtalkback/domain/Member.java
+++ src/main/java/btongtong/btongtalkback/domain/Member.java
@@ -8,7 +8,6 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.BatchSize;
 
 import java.util.ArrayList;
 import java.util.List;
