df9fbea5e3a65b3fa1550955d62ac5609141642c
btongtong <leeym2615@naver.com>
2024-11-25 20:59:28 +0900

    refactor: Extract methods in SecurityConfig for better readability

--- src/main/java/btongtong/btongtalkback/config/SecurityConfig.java
+++ src/main/java/btongtong/btongtalkback/config/SecurityConfig.java
@@ -26,42 +26,74 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final CustomAuthenticationHandler customAuthenticationHandler;
-    private final Oauth2UserService oauth2UserService;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
     private final Oauth2SuccessHandler oauth2SuccessHandler;
     private final Oauth2FailureHandler oauth2FailureHandler;
+    private final Oauth2UserService oauth2UserService;
     private final JwtUtil jwtUtil;
     @Value("${domain.name}")
     private String domainName;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .logout(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/reissue", "/api/oauth2/**", "/api/login/**", "/api/categories/**").permitAll()
-                        .anyRequest().authenticated())
-                .exceptionHandling(exceptionHandling -> exceptionHandling
-                        .authenticationEntryPoint(customAuthenticationHandler)
-                        .accessDeniedHandler(new CustomAccessDeniedHandler()))
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .oauth2Login(oauth2 -> oauth2
-                        .authorizationEndpoint(endpoint -> endpoint.baseUri("/api/oauth2/authorization"))
-                        .redirectionEndpoint(endpoint -> endpoint.baseUri("/api/login/oauth2/code/*"))
-                        .userInfoEndpoint(userInfo -> userInfo.userService(oauth2UserService))
-                        .successHandler(oauth2SuccessHandler)
-                        .failureHandler(oauth2FailureHandler))
-                .addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+        configureCors(http);
+        disableDefaults(http);
+        configureSession(http);
+        configureAuthorization(http);
+        configureExceptionHandling(http);
+        configureOauth2Login(http);
+        addJwtFilter(http);
 
         return http.build();
 
     }
 
+    private void configureCors(HttpSecurity http) throws Exception {
+        http.cors(cors -> cors.configurationSource(getCorsConfigurationSource()));
+    }
+
+    private static void disableDefaults(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable);
+    }
+
+    private static void configureSession(HttpSecurity http) throws Exception {
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    }
+
+    private static void configureAuthorization(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/reissue", "/api/oauth2/**", "/api/login/**", "/api/categories/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+        );
+    }
+
+    private void configureExceptionHandling(HttpSecurity http) throws Exception {
+        http.exceptionHandling(exceptionHandling -> exceptionHandling
+                .authenticationEntryPoint(customAuthenticationHandler)
+                .accessDeniedHandler(customAccessDeniedHandler)
+        );
+    }
+
+    private void configureOauth2Login(HttpSecurity http) throws Exception {
+        http.oauth2Login(oauth2 -> oauth2
+                .authorizationEndpoint(endpoint -> endpoint.baseUri("/api/oauth2/authorization"))
+                .redirectionEndpoint(endpoint -> endpoint.baseUri("/api/login/oauth2/code/*"))
+                .userInfoEndpoint(userInfo -> userInfo.userService(oauth2UserService))
+                .successHandler(oauth2SuccessHandler)
+                .failureHandler(oauth2FailureHandler)
+        );
+    }
+
+    private void addJwtFilter(HttpSecurity http) {
+        http.addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+    }
+
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+    public CorsConfigurationSource getCorsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.addAllowedOrigin(domainName);
         corsConfiguration.addAllowedMethod("*");
