df75a6dd1be07e004f9be2f90980b1f55e525269
btongtong <leeym2615@naver.com>
2024-11-25 21:20:02 +0900

    refactor: Simplify constructor logic by adding @NotNull annotations

--- src/main/java/btongtong/btongtalkback/domain/Record.java
+++ src/main/java/btongtong/btongtalkback/domain/Record.java
@@ -3,11 +3,11 @@ package btongtong.btongtalkback.domain;
 import btongtong.btongtalkback.constant.RecordStatus;
 import com.github.f4b6a3.tsid.TsidCreator;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 
 @Entity
@@ -40,19 +40,15 @@ public class Record {
     }
 
     @Builder
-    public Record(Member member, Flashcard flashcard, RecordStatus status, Boolean progress) {
+    public Record(@NotNull Member member, @NotNull Flashcard flashcard, RecordStatus status, Boolean progress) {
         this.member = member;
         this.flashcard = flashcard;
         this.status = status;
         this.recordDate = LocalDateTime.now();
         this.progress = progress;
 
-        if(member != null) {
-            member.getRecords().add(this);
-        }
-        if(flashcard != null) {
-            flashcard.getRecords().add(this);
-        }
+        member.getRecords().add(this);
+        flashcard.getRecords().add(this);
     }
 
     public void updateStatus(RecordStatus status) {
